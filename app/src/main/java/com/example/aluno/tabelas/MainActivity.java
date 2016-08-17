b9bf7e6b96c9b9fdccc979e20596d24ac2182101
Jotacisio <jotacisio@gmail.com>
2016-08-17 15:19:44 -0300

    Criando Layout via Java

--- app/src/main/java/com/example/aluno/tabelas/MainActivity.java
+++ app/src/main/java/com/example/aluno/tabelas/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.aluno.tabelas;
 
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+/*public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+}*/
+
+public class MainActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle icicle){
+        super.onCreate(icicle);
+
+        //Criar o layout
+        LinearLayout layout = new LinearLayout(this);
+        layout.setPadding(10,10,10,10);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.setLayoutParams(new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.setPadding(10,10,10,10);//pixels
+
+        TextView nome = new TextView(this);
+        nome.setText("Nome:");
+        nome.setLayoutParams(new ActionBar.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+        layout.addView(nome);
+        setContentView(layout);
     }
 }
